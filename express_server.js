cac3104976341bdc788f69b2569d108e52bea184
Chibu <atuchibueze@gmail.com>
2022-04-18 14:52:17 -0400

    Create server using express

---
+++ express_server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+// for a get request of / this server is sending hello
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+// setting up the server to listen to request from the client
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
