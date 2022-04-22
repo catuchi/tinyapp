993bf53231fa075a061fe4dabfaa56f74574806f
Chibu <atuchibueze@gmail.com>
2022-04-22 07:50:39 -0400

    refactor

--- express_server.js
+++ express_server.js
@@ -4,8 +4,9 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 // const cookieParser = require('cookie-parser')
-const cookieSession = require('cookie-session')
-const { generateRandomString } = require("./shortURL");
+const cookieSession = require('cookie-session');
+const { generateRandomString, urlsForUser, findUserByEmail } = require("./helperFunctions");
+const { findUserId } = require("./helperFunctions");
 
 // middleware used to make data received from POST requests to be readable
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,97 +23,48 @@ app.use(cookieSession({
 app.set("view engine", "ejs");
 
 
-// const urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// };
-
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
-    userID: "2"
+    userID: "nkzSdf"
   },
   i3BoGr: {
     longURL: "https://www.google.ca",
-    userID: "1"
+    userID: "nkzSdf"
+  },
+  b2xVn2: {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "nkzSdf"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "nkzSdf"
   }
 };
-const urlsForUser = (id) => {
-  const result = {};
-  for (let shorturl in urlDatabase) {
-    if (urlDatabase[shorturl].userID === id) {
-      result[shorturl] = urlDatabase[shorturl];
-    }
-  }
-  return result;
-}
 
-// const users = { 
-//   "user1": {
-//     id: "1", 
-//     email: "user@example.com", 
-//     password: "purple-monkey-dinosaur"
-//   },
-//  "user2": {
-//     id: "2", 
-//     email: "user2@example.com", 
-//     password: "dishwasher-funk"
-//   },
-//  "user3": {
-//     id: "3", 
-//     email: "user3@example.com", 
-//     password: "dishwasher-funk"
-//   }
-// };
 
-const users = {
-  1: { id: 1, email: 'obiwan@gmail.com', password: 'helloThere' },
-  2: { id: 2, email: 'gimli@gmail.com', password: 'andMyAxe' },
-  3: { id: 3, email: 'a@b.com', password: '123' },
+const users = { 
+  "user1": {
+    id: "user1", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2": {
+    id: "user2", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  },
+ "user3": {
+    id: "user3", 
+    email: "user3@example.com", 
+    password: "dishwasher-funk"
+  },
   nkzSdf: {
     id: 'nkzSdf',
     email: 'bigfish@ocean.ca',
     password: '$2a$10$qeJUBb5zboOqPTLiCI.30eRqqw2r4rhH1VnUs0JuNGjwxYm.aM7Fa'
   }
 };
-
-const findUserByEmail = (newUserEmail, database) => {
-  // const givenEmail = email;
-  for (let user in database) {
-    // if givenEmail == the current user im looping through, then i found the right user
-    if (database[user].email === newUserEmail) {
-      console.log("the email:", database[user].email, "given email:", newUserEmail);
-      // that means i found the right user
-      return database[user];
-      //  badd, dont do this line below !
-      // return res.redirect(`/${userKey}`);
-    }
-  }
-  return false;
-}
-const findUserId = (newUserEmail, database) => {
-  for (let user in database) {
-    if (database[user].email === newUserEmail) {
-      return database[user].id;
-    }
-  }
-  return false;
-}
-const findUser = (newUserEmail, newUserPw, database) => {
-  for (let user in database) {
-    if (database[user].email === newUserEmail) {
-      if (bcrypt.compareSync(newUserPw, database[user].password)) {
-
-        return database[user];
-      }
-      // if (database[user].password === newUserPw) {
-      //   return database[user];
-      // }
-    }
-
-  }
-  return false;
-}
 
 
 // for a get request of / this server is sending hello
@@ -130,8 +82,8 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  console.log("sess",req.session.user_id)
-  console.log("cook",req.cookies)
+  console.log("sess", req.session.user_id)
+  console.log("cook", req.cookies)
 
   if (req.session.user_id === undefined) {
     const templateVars = { user: null, urls: null };
@@ -171,8 +123,6 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   const user = users[req.session.user_id];
-  console.log("anoda--",req.session.user_id)
-  console.log(users)
 
   if (user) {
 
@@ -202,12 +152,9 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.post("/urls", (req, res) => {
   const randomStr = generateRandomString();
-  // console.log("body--",req.body)
   urlDatabase[randomStr] = randomStr
-  // console.log("id--",req.session.user_id)
-  urlDatabase[randomStr] = { longURL: req.body.longURL, userID: req.session.user_id};
+  urlDatabase[randomStr] = { longURL: req.body.longURL, userID: req.session.user_id };
   // urlDatabase[randomStr].longURL = req.body.longURL;
-  // console.log(req.body.longURL);
   res.redirect(`/urls/${randomStr}`);
 });
 
@@ -263,13 +210,6 @@ app.post("/login", (req, res) => {
     res.cookie("user_id", req.session.user_id);
     res.redirect("/urls");
   }
-
-
-
-  // console.log(req.body)
-  // const user = req.body.username;
-  // res.cookie("username", user);
-  // res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
@@ -282,7 +222,6 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  // console.log(req.body);
   const newUserEmail = req.body.email;
   const newUserPassword = bcrypt.hashSync(req.body.password, 10);
   const newUserId = generateRandomString();
@@ -291,7 +230,6 @@ app.post("/register", (req, res) => {
     res.status(400).send("Please enter valid email or password");
     res.end();
   }
-  // console.log(users) 
   const user = findUserByEmail(newUserEmail, users);
   if (user) {
     res.status(400).send("Email already exist please enter new email");
@@ -302,21 +240,11 @@ app.post("/register", (req, res) => {
     res.cookie("user_id", req.session.user_id);
     res.redirect("/urls");
   }
-  // console.log("pw:", newUserPassword, "data", users)
-  // if (user) {
-  //   res.cookie("user_id", user.id);
-  //   return res.redirect("/urls");
-  // }
-
-
-  // res.send('Error, bad email, or pass!');
 });
 
 app.get("/login", (req, res) => {
   res.render("newLoginPage");
 });
-
-
 
 
 // setting up the server to listen to request from the client
---
+++ helperFunctions.js
@@ -0,0 +1,60 @@
+function generateRandomString() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const length = 6;
+  let result = '';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
+
+const urlsForUser = (id) => {
+  const result = {};
+  for (let shorturl in urlDatabase) {
+    if (urlDatabase[shorturl].userID === id) {
+      result[shorturl] = urlDatabase[shorturl];
+    }
+  }
+  return result;
+};
+
+const findUserByEmail = (newUserEmail, database) => {
+  for (let user in database) {
+    if (database[user].email === newUserEmail) {
+      console.log("the email:", database[user].email, "given email:", newUserEmail);
+      return database[user];
+    }
+  }
+  return false;
+};
+
+const findUserId = (newUserEmail, database) => {
+  for (let user in database) {
+    if (database[user].email === newUserEmail) {
+      return database[user].id;
+    }
+  }
+  return false;
+};
+
+const findUser = (newUserEmail, newUserPw, database) => {
+  for (let user in database) {
+    if (database[user].email === newUserEmail) {
+      if (bcrypt.compareSync(newUserPw, database[user].password)) {
+        return database[user];
+      }
+    }
+  }
+  return false;
+};
+
+module.exports = {
+  generateRandomString,
+  urlsForUser,
+  findUserByEmail,
+  findUserId,
+  findUser
+}
